4c59059c8f738610be9ade25f2fd7152a2464941
Marc Ducobu <marc.ducobu@gmail.com>
2014-01-23 18:51:42 +0100

    refactoring description_text_display.js to pass JSHint

--- web/js/description_text_display.js
+++ web/js/description_text_display.js
@@ -1,181 +1,188 @@
+/* jslint vars: true */
+/*jslint indent: 3 */
+/* global define */
+'use strict';
+
 define(['jQuery','map_display','user','descriptions','photo','params','description_edit','comments','basic_data_and_functions'],
-        function($,map_display,user,descriptions,photo,params,description_edit,comments,basic_data_and_functions) {
-	var color_trad_text = {};
-	color_trad_text['0'] = 'pas encore pris en compte (blanc)';
-	color_trad_text['-1'] = 'rejeté (gris)';
-	color_trad_text['1'] = 'pris en compte (rouge)';
-	color_trad_text['2'] = 'en cours de résolution (orange)';
-	color_trad_text['3'] = 'résolu (vert)';
-	var current_description_id = null;
+      function($,map_display,user,descriptions,photo,params,description_edit,comments,basic_data_and_functions) {
+   var color_trad_text = {};
+   color_trad_text['0'] = 'pas encore pris en compte (blanc)';
+   color_trad_text['-1'] = 'rejeté (gris)';
+   color_trad_text['1'] = 'pris en compte (rouge)';
+   color_trad_text['2'] = 'en cours de résolution (orange)';
+   color_trad_text['3'] = 'résolu (vert)';
+   var current_description_id = null;
 
-	function activate_comments_mode() {
-		/**
-		* Display description with the 'comments mode'
-		*/
-        map_display.translate(current_description_id);
-    	$("#div_last_private_comment_container").hide();
-    	$("#span_plus_de_commenaitres_link").hide();
-    	$("#div_list_private_comment_container").show();
-    	$("#div_form_commentaires_cem_gestionnaire").show();
-    	$("#add_new_description_form__message").val("");
-    	scroll(0,0);
-	}
+   function activate_comments_mode() {
+      /**
+      * Display description with the 'comments mode'
+      */
+      map_display.translate(current_description_id);
+      $('#div_last_private_comment_container').hide();
+      $('#span_plus_de_commenaitres_link').hide();
+      $('#div_list_private_comment_container').show();
+      $('#div_form_commentaires_cem_gestionnaire').show();
+      $('#add_new_description_form__message').val('');
+      scroll(0,0);
+   }
 
-	function display_description_of(id_desc) {
-    	/**
-     	* Function which display some data of the place on the webpage.
-     	To be executed when the user click on a marker on the index page.
-     	* @param {int} id_desc The id of the description.
-     	*/
-        current_description_id = id_desc;
-    	photo.refresh_span_photo(id_desc);
-        $("#link_add_photo").unbind("click");
-        $("#link_add_photo").click(function() { photo.pop_up_add_photo(id_desc); });
+   function display_description_of(id_desc) {
+      /**
+      * Function which display some data of the place on the webpage.
+      To be executed when the user click on a marker on the index page.
+      * @param {int} id_desc The id of the description.
+      */
+      var categories_list,
+         desc_data = descriptions.get_by_id(id_desc);
 
-		var desc_data = descriptions.get_by_id(id_desc);
+      current_description_id = id_desc;
+      photo.refresh_span_photo(id_desc);
+      $('#link_add_photo').unbind('click');
+      $('#link_add_photo').click(function() { photo.pop_up_add_photo(id_desc); });
 
-		categories_list = "";
-  		$.each(desc_data.categories, function(i,c) { 
-  			categories_list = categories_list + c.label; + " "});
+      categories_list = '';
+      $.each(desc_data.categories, function(i,c) {
+         categories_list = categories_list + c.label + ' ';
+      });
 
-	    $('.class_span_place_description_id').each(function() { this.innerHTML = desc_data.id; });
-    	$('.class_span_place_description_loc').each(function() { this.innerHTML = desc_data.addressParts.road; });
-    	$('#input_place_description_id').val(desc_data.id);
-    	$('#span_place_description_signaleur').text(desc_data.creator.label);
-        $('#span_place_description_creation_date').text(basic_data_and_functions.timestamp2date(desc_data.createDate.u));
-    	$('#span_place_description_loc').text(desc_data.addressParts.road);
-    	$('#span_place_description_desc').text(desc_data.description);
+      $('.class_span_place_description_id').each(function() { this.innerHTML = desc_data.id; });
+      $('.class_span_place_description_loc').each(function() { this.innerHTML = desc_data.addressParts.road; });
+      $('#input_place_description_id').val(desc_data.id);
+      $('#span_place_description_signaleur').text(desc_data.creator.label);
+      $('#span_place_description_creation_date').text(basic_data_and_functions.timestamp2date(desc_data.createDate.u));
+      $('#span_place_description_loc').text(desc_data.addressParts.road);
+      $('#span_place_description_desc').text(desc_data.description);
 
 
-    	if(desc_data.moderatorComment != '' || user.isCeM() || user.isAdmin()) {
-        	$('#span_place_description_commentaireCeM').text(desc_data.moderatorComment);
-        	$('#div_container_place_description_commentaireCeM').show();
-    	} else {
-        	$('#span_place_description_commentaireCeM').text('');
-        	$('#div_container_place_description_commentaireCeM').hide();
-    	}
+      if (desc_data.moderatorComment != '' || user.isCeM() || user.isAdmin()) {
+         $('#span_place_description_commentaireCeM').text(desc_data.moderatorComment);
+         $('#div_container_place_description_commentaireCeM').show();
+      } else {
+         $('#span_place_description_commentaireCeM').text('');
+         $('#div_container_place_description_commentaireCeM').hide();
+      }
 
-    	$('#span_place_description_cat').text(categories_list);
+      $('#span_place_description_cat').text(categories_list);
 
-    	if (desc_data.placetype == null){
-        	$('#span_place_description_type').text("pas encore de type assigné");
-    	} else {
-        	$('#span_place_description_type').text(desc_data.placetype.label);
-    	}
-    	
-    	if (desc_data.manager == null) {
-        	$('#span_place_description_gestionnaire').text("pas encore de gestionnaire assigné");
-   		} else {
-        	$('#span_place_description_gestionnaire').text(desc_data.manager.label);
-    	}
+      if (desc_data.placetype == null) {
+         $('#span_place_description_type').text('pas encore de type assigné');
+      } else {
+         $('#span_place_description_type').text(desc_data.placetype.label);
+      }
+      
+      if (desc_data.manager == null) {
+         $('#span_place_description_gestionnaire').text('pas encore de gestionnaire assigné');
+      } else {
+         $('#span_place_description_gestionnaire').text(desc_data.manager.label);
+      }
 
-    	$('#span_place_description_status').text(color_trad_text[0]);
+      $('#span_place_description_status').text(color_trad_text[0]);
 
-    	for (i = 0; i < desc_data.statuses.length; i++) {  
-            if (desc_data.statuses[i].t == params.manager_color) {
-                $('#span_place_description_status').text(color_trad_text[desc_data.statuses[i].v]); 
-            }
-        }
+      for (var i = 0; i < desc_data.statuses.length; i++) {
+         if (desc_data.statuses[i].t == params.manager_color) {
+            $('#span_place_description_status').text(color_trad_text[desc_data.statuses[i].v]);
+         }
+      }
 
-    	description_edit.stop_edition(); // si l'utilisateur a commencé à éditer , il faut cacher les formulaires
-    	display_regarding_to_user_role();
-        
-	    $('#div__place_description_display').show();
-	}
+      description_edit.stop_edition(); // si l'utilisateur a commencé à éditer , il faut cacher les formulaires
+      display_regarding_to_user_role();
+      
+      $('#div__place_description_display').show();
+   }
 
-	function display_editing_button() {
-		/**
-    	* if the user has certain role, he can edit certain information
-    	* this function display or not the button with which we can edit the 
-    	* information
-    	*/
-    	if(user.canModifyCategories() || user.isAdmin()) {
-        	$('#span_place_description_cat_button').show();
-    	} else {
-        	$('#span_place_description_cat_button').hide();
-    	}
+   function display_editing_button() {
+      /**
+      * if the user has certain role, he can edit certain information
+      * this function display or not the button with which we can edit the 
+      * information
+      */
+      if (user.canModifyCategories() || user.isAdmin()) {
+         $('#span_place_description_cat_button').show();
+      } else {
+         $('#span_place_description_cat_button').hide();
+      }
 
-	    if(user.canModifyLittleDetails() || user.isAdmin()) {
-    	    $('#span_place_description_loc_button').show();
-        	$('#span_place_description_desc_button').show();
-    	} else {
-        	$('#span_place_description_loc_button').hide();
-        	$('#span_place_description_desc_button').hide();
-    	}
+      if (user.canModifyLittleDetails() || user.isAdmin()) {
+         $('#span_place_description_loc_button').show();
+         $('#span_place_description_desc_button').show();
+      } else {
+         $('#span_place_description_loc_button').hide();
+         $('#span_place_description_desc_button').hide();
+      }
 
-	    if(user.canModifyPlacetype() || user.isAdmin()) {
-    	    $('#span_place_description_type_button').show();
-    	} else {
-        $('#span_place_description_type_button').hide();
-    	}
+      if (user.canModifyPlacetype() || user.isAdmin()) {
+         $('#span_place_description_type_button').show();
+      } else {
+         $('#span_place_description_type_button').hide();
+      }
 
-	    if(user.canModifyManager() || user.isAdmin()) {
-    	    $('#span_place_description_gestionnaire_button').show();
-    	} else {
-        	$('#span_place_description_gestionnaire_button').hide();
-    	}
+      if (user.canModifyManager() || user.isAdmin()) {
+         $('#span_place_description_gestionnaire_button').show();
+      } else {
+         $('#span_place_description_gestionnaire_button').hide();
+      }
 
-	    if(user.canUnpublishADescription() || user.isAdmin()){
-	        $('#span_place_description_delete_button').show();
-	    } else {
-	        $('#span_place_description_delete_button').hide();
-	    }
+      if (user.canUnpublishADescription() || user.isAdmin()) {
+         $('#span_place_description_delete_button').show();
+      } else {
+         $('#span_place_description_delete_button').hide();
+      }
 
-	    if(user.isCeM() || user.isAdmin()){
-    	    $('#span_place_description_commentaireCeM_button').show();
-    	    $('#span_place_description_status_button').show();
-    	    $('#div_container_place_description_commentaireCeM').show();
-            $('#button_edit_lon_lat').show();
-    	} else {
-    	    $('#span_place_description_commentaireCeM_button').hide();
-    	    $('#span_place_description_status_button').hide();
-            $('#button_edit_lon_lat').hide();
-    	}
+      if (user.isCeM() || user.isAdmin()) {
+         $('#span_place_description_commentaireCeM_button').show();
+         $('#span_place_description_status_button').show();
+         $('#div_container_place_description_commentaireCeM').show();
+         $('#button_edit_lon_lat').show();
+      } else {
+         $('#span_place_description_commentaireCeM_button').hide();
+         $('#span_place_description_status_button').hide();
+         $('#button_edit_lon_lat').hide();
+      }
 
-	    if(user.isAdmin() || user.isCeM() || user.isGdV()) {
-	        $('#div_commentaires_cem_gestionnaire').show();
-	    } else{
-	        $('#div_commentaires_cem_gestionnaire').hide();
-	    }
+      if (user.isAdmin() || user.isCeM() || user.isGdV()) {
+         $('#div_commentaires_cem_gestionnaire').show();
+      } else{
+         $('#div_commentaires_cem_gestionnaire').hide();
+      }
 
-        $('#button_save_lon_lat').hide();
-	}
+      $('#button_save_lon_lat').hide();
+   }
 
-	function display_regarding_to_user_role() {
-		/**
-		* The user can have the right to modify some information or to see personnal data
-		of the creator.
-		*/
-        if(user.isGdV() || user.isCeM() || user.isAdmin()) {
-            comments.update_last(current_description_id);
-            comments.update_all(current_description_id);
-            $("#form_add_new_comment").unbind("click");
-            $("#span_plus_de_commenaitres_link a").click(function(e) {
-                e.preventDefault();
-                activate_comments_mode();
-            });
+   function display_regarding_to_user_role() {
+      /**
+      * The user can have the right to modify some information or to see personnal data
+      of the creator.
+      */
+      if (user.isGdV() || user.isCeM() || user.isAdmin()) {
+         comments.update_last(current_description_id);
+         comments.update_all(current_description_id);
+         $('#form_add_new_comment').unbind('click');
+         $('#span_plus_de_commenaitres_link a').click(function(e) {
+            e.preventDefault();
+            activate_comments_mode();
+         });
 
-            $("#form_add_new_comment").unbind("submit");
-            $("#form_add_new_comment").submit(function(e) {
-                e.preventDefault();
-                comments.submit_creation_form(current_description_id);  
-            });
-        }
+         $('#form_add_new_comment').unbind('submit');
+         $('#form_add_new_comment').submit(function(e) {
+            e.preventDefault();
+            comments.submit_creation_form(current_description_id);
+         });
+      }
 
-		if (user.canVieuwUsersDetails() || user.isAdmin()) {
-	    	var desc_data = descriptions.get_by_id(current_description_id);
-            $('#span_place_description_signaleur_contact').html(' (email : <a href="mailto:'+ desc_data.creator.email +'">'+ 
-        	desc_data.creator.email +'</a>, téléphone : '+ desc_data.creator.phonenumber + ')');
-        } else {
-            $('#span_place_description_signaleur_contact').text('');
-        }
+      if (user.canVieuwUsersDetails() || user.isAdmin()) {
+         var desc_data = descriptions.get_by_id(current_description_id);
+         $('#span_place_description_signaleur_contact').html(' (email : <a href="mailto:'+ desc_data.creator.email +'">'+
+         desc_data.creator.email +'</a>, téléphone : '+ desc_data.creator.phonenumber + ')');
+      } else {
+         $('#span_place_description_signaleur_contact').text('');
+      }
 
-        display_editing_button();
-	}
+      display_editing_button();
+   }
 
-	return {
-		activate_comments_mode: activate_comments_mode,
-		display_description_of: display_description_of,
-		display_regarding_to_user_role: display_regarding_to_user_role
-    };
+   return {
+      activate_comments_mode: activate_comments_mode,
+      display_description_of: display_description_of,
+      display_regarding_to_user_role: display_regarding_to_user_role
+   };
 });
