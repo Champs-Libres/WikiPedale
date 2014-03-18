dbe06c4bb117a5a1c89cc13057580861ed9bdbdb
Marc Ducobu <marc.ducobu@gmail.com>
2014-03-18 14:38:37 +0100

    refactoting : removing useless space

--- src/Progracqteur/WikipedaleBundle/Controller/CategoryController.php
+++ src/Progracqteur/WikipedaleBundle/Controller/CategoryController.php
@@ -3,7 +3,6 @@
 namespace Progracqteur\WikipedaleBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 use Progracqteur\WikipedaleBundle\Entity\Model\Category;
 use Progracqteur\WikipedaleBundle\Form\Model\CategoryType;
 
