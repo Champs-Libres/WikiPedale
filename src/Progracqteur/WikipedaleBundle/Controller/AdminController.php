b8907a9b1e844ceeeaee86aada47b533aeab5e55
Marc Ducobu <marc.ducobu@gmail.com>
2014-03-18 12:07:53 +0100

    Refactoring : removing useless spaces

--- src/Progracqteur/WikipedaleBundle/Controller/AdminController.php
+++ src/Progracqteur/WikipedaleBundle/Controller/AdminController.php
@@ -1,17 +1,13 @@
 <?php
-
 namespace Progracqteur\WikipedaleBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AdminController extends Controller
 {
-
     public function indexAction()
     {
         return $this->render('ProgracqteurWikipedaleBundle:Admin:default.html.twig');
     }
-    
-    
 }
 
