bc2da063fddaba74e6dc341c2d1442b31dfb6d2f
Marc Ducobu <marc@champs-libres.coop>
2014-03-31 12:57:16 +0200

    Debug NormalizerSerializerService::REPORTs_TYPE -> NormalizerSerializerService::REPORT_TYPE

--- src/Progracqteur/WikipedaleBundle/Resources/Normalizer/UserNormalizer.php
+++ src/Progracqteur/WikipedaleBundle/Resources/Normalizer/UserNormalizer.php
@@ -44,7 +44,7 @@ class UserNormalizer implements NormalizerInterface
     public function denormalize($data, $class, $format = null, array $context = array()) {
         //si la classe demandée n'est pas USER, il faut uniquement renvoyer un objet User existant,
         // ou un objet Unregistereduser
-        if ($class === NormalizerSerializerService::REPORTs_TYPE)
+        if ($class === NormalizerSerializerService::REPORT_TYPE)
         {
             if ($data['id'] === null)
             {          
