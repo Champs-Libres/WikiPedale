1cd1ae982fe721b0872ab720b778e18cb8f4b714
Marc Ducobu <marc@champs-libres.coop>
2014-06-03 11:17:35 +0200

    refactoring : indentation

--- web/js/data_map_glue.js
+++ web/js/data_map_glue.js
@@ -35,14 +35,15 @@ define(['jQuery','map_display','report','description_text_display','user','infor
       });
 
       $.when(
-      $.getJSON(jsonUrlData, function(data) {
-         user.update(data.user);
-         report.updateAll(data.results, function () {
-            $.each(data.results, function(index, aPlaceData) {
-               map_display.add_marker(aPlaceData.id, focus_on_place_of);
+         $.getJSON(jsonUrlData, function(data) {
+            user.update(data.user);
+            report.updateAll(data.results, function () {
+               $.each(data.results, function(index, aPlaceData) {
+                  map_display.add_marker(aPlaceData.id, focus_on_place_of);
+               });
             });
-         });
-      })).done( function() {
+         })
+      ).done(function() {
          if(marker_id_to_display) {
             focus_on_place_of(marker_id_to_display);
          }
