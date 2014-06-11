c64f53470077f661ff69a3841b1c1de30bbebc5c
Marc Ducobu <marc@champs-libres.coop>
2014-06-11 16:23:23 +0200

    views/Admin/default.html.twig -> index.html.twig

--- src/Progracqteur/WikipedaleBundle/Controller/AdminController.php
+++ src/Progracqteur/WikipedaleBundle/Controller/AdminController.php
@@ -7,7 +7,7 @@ class AdminController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ProgracqteurWikipedaleBundle:Admin:default.html.twig');
+        return $this->render('ProgracqteurWikipedaleBundle:Admin:index.html.twig');
     }
 }
 
