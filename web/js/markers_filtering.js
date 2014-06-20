291442f6fa238099eebac4e65dc4890c63d2a1e0
Marc Ducobu <marc@champs-libres.coop>
2014-06-20 15:44:09 +0200

    Refactoring JS

--- web/js/markers_filtering.js
+++ web/js/markers_filtering.js
@@ -11,27 +11,34 @@
 */
 
 define(['jQuery', 'map_display', 'report', 'category', 'basic_data_and_functions'], function ($, map_display, report, category, basic_data_and_functions) {
-   var filtering_form_activated = false,  // true iff  displaying the div "filter_and_export_menu" and
-   // the choice of the user (done in the filtering form) has to be considered.
-      mode_activated = {}; // to remember each option of the filtering form has been
-   // choosed by the user
+   var filter = { //to remember for each filtering element if it has been activated, and the data asked
+      // filter.element.enable -> true if the filtering on this element is enable
+      // filted.element.XXX -> data used by the filtering
+      enable : false,  // true iff  displaying the div "filter_and_export_menu". This implies that
+      // the choice of the user (done in the filtering form) has to be considered.
+      manager: {enable: false, to_display: []},
+      timestamp: {enable: false},
+      category: {enable: false, to_display: []},
+      long_term_category: {enable: false},
+      moderator_rejected_status: {enable: false},
+      moderator_status: {enable: false, to_display: []}
+   };
 
-   var timestamp_begin, timestamp_end;
-
-   function changeExportLinkRegardingToFiltering(statusCeM_to_display, id_cat_to_display) {
+   function changeExportLinkRegardingToFiltering() {
       /**
       * Change the csv export link regarding to the filtering options checked
-      * @param {integer Array} statusCeM_to_display An array of the 2ted statusCeM  (select the signalements with its CeM notation in this array)
-      * @param {integer Array} id_cat_to_display An array of the selected categories (select the signalements with its category in this array)
       */
-      var csv_basic_export_link = $('#csv_basic_export_link').attr('href'),
-         csv_export_link;
-      csv_export_link = csv_basic_export_link + '&categories=' + id_cat_to_display.join(',');
-      csv_export_link = csv_export_link + '&notations=' + statusCeM_to_display.join(',');
+      // no need to check if filter.enable -> otherwise not displayed
+      var csv_export_link = $('#csv_basic_export_link').attr('href') + '&categories=' + filter.category.to_display.join(',') +
+         '&notations=' + filter.moderator_status.to_display.join(',');
 
-      if(mode_activated.timestamp) {
-         csv_export_link = csv_export_link + '&timestamp_begin=' + timestamp_begin;
-         csv_export_link = csv_export_link + '&timestamp_end=' + timestamp_end;
+      if(filter.manager.enable) {
+         csv_export_link = csv_export_link + '&managers=' + filter.manager.to_display.join(',');
+      }
+
+      if(filter.timestamp.enable) {
+         csv_export_link = csv_export_link + '&timestamp_begin=' +
+            filter.timestamp.begin + '&timestamp_end=' + filter.timestamp.end;
       }
 
       $('#csv_export_link').attr('href', csv_export_link);
@@ -42,70 +49,74 @@ define(['jQuery', 'map_display', 'report', 'category', 'basic_data_and_functions
       * Display on the map the markers regarding to the selection made by the user
       * via the filtering form (or not if not activated).
       */
-      var id_cat_to_display = [], //the id of the categories that will be displayed on the map
-         moderator_status_to_display = [], //the moderator status that will be displayed on the map
-         id_manager_to_display = [];
-
+      
       // Short term and medium categories
-      if (mode_activated.category && filtering_form_activated) {
-         id_cat_to_display = $('#filtering__category__value_children').select2('val').map(function(s) {return parseInt(s);});
+      if (filter.category.enable && filter.enable) {
+         filter.category.to_display = $('#filtering__category__value_children').select2('val').map(function(s) {return parseInt(s);});
       } else {
+         filter.category.to_display = [];
          category.getAll(function(categories) {
             $.each(categories, function(i,cat) {
                if(cat.children.length > 0) {
                   $.each(cat.children, function(i, child) {
                      if(child.term !== 'long') {
-                        id_cat_to_display.push(child.id);
+                        filter.category.to_display.push(child.id);
                      }
                   });
                }
                if(cat.children.length === 0 && cat.term !== 'long') {
-                  id_cat_to_display.push(cat.id);
+                  filter.category.to_display.push(cat.id);
                }
             });
          });
       }
 
       // Long term categories
-      if (mode_activated.long_term_category && filtering_form_activated) {
+      if (filter.long_term_category.enable && filter.enable) {
          $.each($('#filtering__long_term_category__value_children').select2('val'), function (index, id_cat) {
-            id_cat_to_display.push(parseInt(id_cat));
+            filter.category.to_display.push(parseInt(id_cat));
          });
       }
 
-      // White, Red, Yellow, Green statuses (Moderator status)
-      if (mode_activated.moderator_status && filtering_form_activated) {
-         moderator_status_to_display = $('#filtering__moderator_status__value').select2('val').map(function(s) {return parseInt(s);});
+      // Moderator status (white, red, yellow or green)
+      if (filter.moderator_status.enable && filter.enable) {
+         filter.moderator_status.to_display = $('#filtering__moderator_status__value').select2('val').map(function(s) {return parseInt(s);});
       } else {
          $('#filtering__moderator_status__value option').each(function (i, v) {
-            moderator_status_to_display.push(parseInt(v.value));
+            filter.moderator_status.to_display.push(parseInt(v.value));
          });
       }
 
       // Gray (rejected) status
-      if (mode_activated.moderator_rejected_status && filtering_form_activated) {
-         moderator_status_to_display.push(-1);
+      if (filter.moderator_rejected_status.enable && filter.enable) {
+         filter.moderator_status.to_display.push(-1);
       }
 
-      // Manager
-      if (mode_activated.manager && filtering_form_activated) {
-         id_manager_to_display = $('#filtering__manager__value').select2('val').map(function(s) {return parseInt(s);});
-      }
+      if(filter.enable) {
+         // Manager
+         if (filter.manager.enable) {
+            filter.manager.to_display = $('#filtering__manager__value').select2('val').map(function(s) {return parseInt(s);});
+         }
 
-      //timestamp filtering
-      if (mode_activated.timestamp && filtering_form_activated) {
-         timestamp_begin = basic_data_and_functions.stringDate2UnixTimestamp($('#filtering__timestamp__value_begin').val(),0); //0 is set if no value
-         timestamp_end = basic_data_and_functions.stringDate2UnixTimestamp($('#filtering__timestamp__value_end').val(), (((new Date()).getTime() / 1000)) ,true); //((new Date()).getTime() / 1000) is set if no value
+         //timestamp filtering
+         if (filter.timestamp.enable) {
+            filter.timestamp.begin = basic_data_and_functions.stringDate2UnixTimestamp($('#filtering__timestamp__value_begin').val(),0); //0 is set if no value
+            filter.timestamp.end = basic_data_and_functions.stringDate2UnixTimestamp($('#filtering__timestamp__value_end').val(), (((new Date()).getTime() / 1000)) ,true); //((new Date()).getTime() / 1000) is set if no value
+         }
+
       }
 
       $.each(report.getAll(), function (desc_id, desc_data) {
          if (typeof desc_data !== undefined) {
-
             // desc_data does not have a status of type cem it has to be considered as 0 (not considered)
-            if ($.inArray(parseInt(report.getStatus('cem', desc_data, 0)),moderator_status_to_display) !== -1 &&
-               $.inArray(parseInt(desc_data.category.id),id_cat_to_display) !== -1 &&
-               (!mode_activated.timestamp || !filtering_form_activated || (desc_data.createDate.u > timestamp_begin && desc_data.createDate.u < timestamp_end)) &&
-               (!mode_activated.manager || !filtering_form_activated || $.inArray(report.getManagerId(desc_data,-1),id_manager_to_display) !== -1)
+            if ($.inArray(parseInt(report.getStatus('cem', desc_data, 0)),filter.moderator_status.to_display) !== -1 &&
+                  $.inArray(parseInt(desc_data.category.id),filter.category.to_display) !== -1 &&
+                  (
+                     !filter.enable || (
+                        (!filter.timestamp.enable || (desc_data.createDate.u > filter.timestamp.begin && desc_data.createDate.u < filter.timestamp.end)) &&
+                        (!filter.manager.enable || $.inArray(report.getManagerId(desc_data,-1),filter.manager.to_display ) !== -1)
+                     )
+                  )
                ) {
                map_display.display_marker(desc_id);
             } else {
@@ -114,15 +125,15 @@ define(['jQuery', 'map_display', 'report', 'category', 'basic_data_and_functions
          }
       });
 
-      changeExportLinkRegardingToFiltering(moderator_status_to_display, id_cat_to_display);
+      changeExportLinkRegardingToFiltering();
    }
 
    function activateUnactivateFilteringForm() {
       /**
-      * Function used to signal that the user wants to activate/unactivate the filtering mode.
+      * Function used to signal that the user wants to activate/unactivate the filtering.
       * This function will display/undisplay the filtering form.
       */
-      if (filtering_form_activated) {
+      if (filter.enable) {
          $('#stop_filter_and_export_button').hide();
          $('#filter_and_export_button').show();
          $('#filter_and_export_menu').hide();
@@ -131,28 +142,28 @@ define(['jQuery', 'map_display', 'report', 'category', 'basic_data_and_functions
          $('#filter_and_export_button').hide();
          $('#filter_and_export_menu').show();
       }
-      filtering_form_activated = !filtering_form_activated;
+      filter.enable = !filter.enable;
       displayMarkersRegardingToFiltering();
    }
 
    function initFor(element) {
+      /**
+      * init the filtering for the element {element}.
+      * @param{string} element The element to filter (or not) (see var filter)
+      */
       if(element === 'manager') {
          $.each(report.getAllManagers(), function(i, e) {
             $('#filtering__' + element + '__value').append(
                '<option value="' +  e.manager.id +   '">' +  e.manager.label +   '</option>');
          });
-      }
-
-      if(element === 'category') {
+      } else if(element === 'category') {
          category.insertParentCategoryToSelectField('#filtering__category__value_parent', ['short','medium']);
          $('#filtering__category__value_parent').on('select2-selecting', function(e) {
             category.setChildrenToSelect2Filed('#filtering__category__value_children',e.val, ['short','medium']);
             $('#filtering__category__value_children').on('select2-selecting', displayMarkersRegardingToFiltering);
             displayMarkersRegardingToFiltering();
          });
-      }
-
-      if(element === 'long_term_category') {
+      } else if(element === 'long_term_category') {
          category.insertParentCategoryToSelectField('#filtering__long_term_category__value_parent', ['long']);
          $('#filtering__long_term_category__value_parent').on('select2-selecting', function(e) {
             category.setChildrenToSelect2Filed('#filtering__long_term_category__value_children',e.val, ['long']);
@@ -165,7 +176,6 @@ define(['jQuery', 'map_display', 'report', 'category', 'basic_data_and_functions
          $('#filtering__' + element + '__checkbox').removeAttr('checked');
       }
 
-      mode_activated[element] = false;
       $('#filtering__' + element + '__form_div select').select2();
       $('#filtering__' + element + '__checkbox').on('change', function() { changeModeFor(element); });
 
@@ -174,13 +184,17 @@ define(['jQuery', 'map_display', 'report', 'category', 'basic_data_and_functions
    }
 
    function changeModeFor(element) {
-      if (mode_activated[element]) {
+      /**
+      * Enable / Disable the filtering for the element {element}.
+      * @param{string} element The element to filter (or not) (see var filter)
+      */
+      if (filter[element].enable) {
          $('#filtering__' + element + '__form_div').hide();
       }
       else {
          $('#filtering__' + element + '__form_div').show();
       }
-      mode_activated[element] = !mode_activated[element];
+      filter[element].enable = !filter[element].enable;
       displayMarkersRegardingToFiltering();
    }
 
