dc9f53e820614b51f97bdc466609607b98af12c1
Marc Ducobu <marc@champs-libres.coop>
2014-07-01 15:47:55 +0200

    Re-indentation

--- src/Progracqteur/WikipedaleBundle/Entity/Management/User.php
+++ src/Progracqteur/WikipedaleBundle/Entity/Management/User.php
@@ -16,398 +16,381 @@ use Doctrine\Common\Collections\ArrayCollection;
  */
 class User extends BaseUser
 {
+   protected $email = '';
 
-    protected $email = '';
+   /**
+    * the way the user want to be publicly known
+    * 
+    * @var string $label
+    */
+   protected $label = '';
 
-    /**
-     * the way the user want to be publicly known
-     * 
-     * @var string $label
-     */
-    protected $label = '';
-    
-    /**
-     *
-     * @var string 
-     */
-    protected $phonenumber = "";
-    
+   /**
+    *
+    * @var string 
+    */
+   protected $phonenumber = "";
 
+   /**
+    * @var datetime $creationDate
+    */
+   protected $creationDate;
 
-    /**
-     * @var datetime $creationDate
-     */
-    protected $creationDate;
+   /**
+    * @var Progracqteur\WikipedaleBundle\Resources\Container\Hash $infos
+    */
+   private $infos;
 
-    /**
-     * @var Progracqteur\WikipedaleBundle\Resources\Container\Hash $infos
-     */
-    private $infos;
+   /**
+    * @var integer $nbComment
+    */
+   private $nbComment = 0;
 
-    /**
-     * @var integer $nbComment
-     */
-    private $nbComment = 0;
+   /**
+    * @var integer $nbVote
+    */
+   private $nbVote = 0;
 
-    /**
-     * @var integer $nbVote
-     */
-    private $nbVote = 0;
-    
-    /**
-     *
-     * @var boolean 
-     */
-    private $virtual = false;
-    
-    /**
-     *
-     * @var Doctrine\Common\Collections\ArrayCollection
-     */
-    private $notificationSubscriptions;
-    
-    
-    const ROLE_ADMIN = 'ROLE_ADMIN';
-    
-    /**
-     * indicate if the user may create or alter categories on a report
-     * 
-     * @var string
-     */
-    const ROLE_CATEGORY = 'ROLE_CATEGORY'; 
-    
-    /**
-     * indicate if the user may create or alter notation. 
-     * The group which use it must be associated with a particular notation
-     * 
-     * @var string
-     */
-    const ROLE_NOTATION = 'ROLE_NOTATION';
-    
-    /**
-     * @var string
-     */
-    const ROLE_MANAGER = 'ROLE_MANAGER';
-    
-    /**
-     * @var string
-     */
-    const ROLE_MODERATOR = 'ROLE_MODERATOR';
-    
-    /**
-     * indicate if the user may alter details of a little point
-     * 
-     * @var string
-     */
-    const ROLE_DETAILS_LITTLE = 'ROLE_DETAILS_LITTLE';
-    
-    /**
-     * indicate if the user may alter details of a BIG point
-     * 
-     * @var string
-     */
-    const ROLE_DETAILS_BIG = 'ROLE_DETAILS_BIG';
-    
-    /**
-     * 
-     * indicate if the user may publish/unpublish a report
-     * 
-     * @var string 
-     */
-    const ROLE_PUBLISHED = 'ROLE_PUBLISHED';
-    
-    /**
-     * indicate if the user may see email and personal
-     * details of other users
-     * 
-     * @var string
-     */
-    const ROLE_SEE_USER_DETAILS = 'ROLE_SEE_USER_DETAILS';
-    
-    /**
-     * indicate if the user may alter or modify the manager
-     * of a report
-     * 
-     * @var string
-     */
-    const ROLE_MANAGER_ALTER = 'ROLE_MANAGER_ALTER';
-    
-    /**
-     * indicate if the user may alter the report type
-     */
-    const ROLE_REPORTTYPE_ALTER = 'ROLE_PLACETYPE_ALTER';
-    
-    /**
-     * indicate if the user may see unaccepted entities (like reports,
-     *  photos, comments, etc.)
-     */
-    const ROLE_SEE_UNACCEPTED = 'ROLE_SEE_UNACCEPTED';
-    
-    /**
-     * indicate if the user may alter the moderator's comment
-     * the moderator's comment is inside a report: this is not a comment entity
-     */
-    const ROLE_MODERATOR_COMMENT_ALTER = 'ROLE_MODERATOR_COMMENT_ALTER';
-    
-    /**
-     * indicate if the user may add and see private comments between
-     * moderators and manager. This concern comment entity and not 
-     * report's moderator comment
-     */
-    const ROLE_COMMENT_MODERATOR_MANAGER = 'ROLE_COMMENT_MODERATOR_MANAGER';
-    
-    /**
-     * indicate if the user may alter the term.
-     */
-    const ROLE_REPORT_TERM = 'ROLE_PLACE_TERM';
-    
-    
-    
-    public function __construct()
-    {
-        parent::__construct();
-        $this->setCreationDate(new \DateTime());
-        $this->infos = new Hash();
-        $salt = md5( uniqid(rand(0,1000), true) );
-        $this->setSalt($salt);
-        $this->notificationSubscriptions = new ArrayCollection();
-    }
+   /**
+    *
+    * @var boolean 
+    */
+   private $virtual = false;
 
+   /**
+    *
+    * @var Doctrine\Common\Collections\ArrayCollection
+    */
+   private $notificationSubscriptions;
 
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+   const ROLE_ADMIN = 'ROLE_ADMIN';
 
+   /**
+    * indicate if the user may create or alter categories on a report
+    * 
+    * @var string
+    */
+   const ROLE_CATEGORY = 'ROLE_CATEGORY'; 
 
+   /**
+    * indicate if the user may create or alter notation. 
+    * The group which use it must be associated with a particular notation
+    * 
+    * @var string
+    */
+   const ROLE_NOTATION = 'ROLE_NOTATION';
 
+   /**
+    * @var string
+    */
+   const ROLE_MANAGER = 'ROLE_MANAGER';
 
-    /**
-     * Set label
-     * @param string $label
-     */
-    public function setLabel($label)
-    {
-        $this->label = $label;
-    }
+   /**
+    * @var string
+    */
+   const ROLE_MODERATOR = 'ROLE_MODERATOR';
 
-    /**
-     * Get label
-     *
-     * @return string 
-     */
-    public function getLabel()
-    {
-        return $this->label;
-    }
+   /**
+    * indicate if the user may alter details of a little point
+    * 
+    * @var string
+    */
+   const ROLE_DETAILS_LITTLE = 'ROLE_DETAILS_LITTLE';
 
-    /**
-     * Set salt
-     *
-     * @param string $salt
-     */
-    private function setSalt($salt)
-    {
-        $this->salt = $salt;
-    }
-    
-    public function getSalt()
-    {
-        return '';
-    }
+   /**
+    * indicate if the user may alter details of a BIG point
+    * 
+    * @var string
+    */
+   const ROLE_DETAILS_BIG = 'ROLE_DETAILS_BIG';
 
+   /**
+    * 
+    * indicate if the user may publish/unpublish a report
+    * 
+    * @var string 
+    */
+   const ROLE_PUBLISHED = 'ROLE_PUBLISHED';
 
+   /**
+    * indicate if the user may see email and personal
+    * details of other users
+    * 
+    * @var string
+    */
+   const ROLE_SEE_USER_DETAILS = 'ROLE_SEE_USER_DETAILS';
 
-    /**
-     * Set creationDate
-     *
-     * @param datetime $creationDate
-     */
-    protected function setCreationDate($creationDate)
-    {
-        $this->creationDate = $creationDate;
-    }
+   /**
+    * indicate if the user may alter or modify the manager
+    * of a report
+    * 
+    * @var string
+    */
+   const ROLE_MANAGER_ALTER = 'ROLE_MANAGER_ALTER';
 
-    /**
-     * Get creationDate
-     *
-     * @return datetime 
-     */
-    public function getCreationDate()
-    {
-        return $this->creationDate;
-    }
+   /**
+    * indicate if the user may alter the report type
+    */
+   const ROLE_REPORTTYPE_ALTER = 'ROLE_PLACETYPE_ALTER';
 
-    /**
-     * Set confirmed
-     *
-     * @param boolean $confirmed
-     */
-    public function setConfirmed($confirmed)
-    {
-        $this->confirmed = $confirmed;
-    }
+   /**
+    * indicate if the user may see unaccepted entities (like reports,
+    *  photos, comments, etc.)
+    */
+   const ROLE_SEE_UNACCEPTED = 'ROLE_SEE_UNACCEPTED';
 
-    /**
-     * Get confirmed
-     *
-     * @return boolean 
-     */
-    public function getConfirmed()
-    {
-        return $this->confirmed;
-    }
-    
-    /**
-     * 
-     * @param string $phonenumber
-     * @return \Progracqteur\WikipedaleBundle\Entity\Management\User
-     */
-    public function setPhonenumber($phonenumber)
-    {
-        $this->phonenumber = $phonenumber;
-        return $this;
-    }
-    
-    /**
-     * 
-     * @return string
-     */
-    public function getPhonenumber()
-    {
-        return $this->phonenumber;
-    }
-    
+   /**
+    * indicate if the user may alter the moderator's comment
+    * the moderator's comment is inside a report: this is not a comment entity
+    */
+   const ROLE_MODERATOR_COMMENT_ALTER = 'ROLE_MODERATOR_COMMENT_ALTER';
 
-    /**
-     * Set infos
-     *
-     * @param Progracqteur\WikipedaleBundle\Resources\Container\Hash $infos
-     */
-    public function setInfos(Hash $infos)
-    {
-        $this->infos = $infos;
-    }
+   /**
+    * indicate if the user may add and see private comments between
+    * moderators and manager. This concern comment entity and not 
+    * report's moderator comment
+    */
+   const ROLE_COMMENT_MODERATOR_MANAGER = 'ROLE_COMMENT_MODERATOR_MANAGER';
 
-    /**
-     * Get infos
-     *
-     * @return Progracqteur\WikipedaleBundle\Resources\Container\Hash 
-     */
-    public function getInfos()
-    {
-        return $this->infos;
-    }
+   /**
+    * indicate if the user may alter the term.
+    */
+   const ROLE_REPORT_TERM = 'ROLE_PLACE_TERM';
 
-    /**
-     * Set nbComment
-     *
-     * 
-     */
-    public function increaseNbComment()
-    {
-        $this->nbComment++;
-    }
+   public function __construct()
+   {
+      parent::__construct();
+      $this->setCreationDate(new \DateTime());
+      $this->infos = new Hash();
+      $salt = md5( uniqid(rand(0,1000), true) );
+      $this->setSalt($salt);
+      $this->notificationSubscriptions = new ArrayCollection();
+   }
 
-    /**
-     * Get nbComment
-     *
-     * @return integer 
-     */
-    public function getNbComment()
-    {
-        return $this->nbComment;
-    }
+   /**
+    * Get id
+    *
+    * @return integer 
+    */
+   public function getId()
+   {
+      return $this->id;
+   }
 
-    /**
-     * Set nbVote
-     *
-     * 
-     */
-    public function increaseNbVote($nbVote)
-    {
-        $this->nbVote++;
-    }
+   /**
+    * Set label
+    * @param string $label
+    */
+   public function setLabel($label)
+   {
+      $this->label = $label;
+   }
 
-    /**
-     * Get nbVote
-     *
-     * @return integer 
-     */
-    public function getNbVote()
-    {
-        return $this->nbVote;
-    }
-    
-        public function isRegistered()
-    {
-        return true;
-    }
-    
-    public function setVirtual($virtual) {
-        $this->virtual = $virtual;
-        $this->setLocked($virtual);
-    }
-    
-    public function isVirtual() {
-        return $this->virtual;
-    }
-    
-    public function isVirtualConsistant(\Symfony\Component\Validator\ExecutionContextInterface $context) {
-        if ($this->isVirtual() === true ) {
-            if ($this->isLocked() === false ) {
-                $context->addViolationAt('locked', 
-                        "admin.profile_user.inconsistent_virtual_lock", array(), null);
-            }
-        }
-    }
-    
-    /**
-     * 
-     * @return Doctrine\Common\Collections\ArrayCollection
-     */
-    public function getNotificationSubscriptions()
-    {
-        return $this->notificationSubscriptions;
-    }
+   /**
+    * Get label
+    *
+    * @return string 
+    */
+   public function getLabel()
+   {
+      return $this->label;
+   }
 
-    public function equals(UserInterface $user) {
-        if ($user instanceof UnregisteredUser)
-            return false;
-        else {
-            return $user->getId() === $this->getId();
+   /**
+    * Set salt
+    *
+    * @param string $salt
+    */
+   private function setSalt($salt)
+   {
+      $this->salt = $salt;
+   }
 
-        }
-    }
-    
-    /**
-     * 
-     * @param \Progracqteur\WikipedaleBundle\Entity\Management\NotificationSubscription $notification
-     * @return \Progracqteur\WikipedaleBundle\Entity\Management\User
-     */
-    public function addNotificationSubscription(NotificationSubscription $notification)
-    {
-        $this->notificationSubscriptions->add($notification);
-        return $this;
-    }
-    
-    public function removeNotificationSubscription(NotificationSubscription $notification)
-    {
-        foreach($this->notificationSubscriptions as $key => $not)
-        {
-            if ($not->getId() === $notification->getId())
-            {
-                $this->notificationSubscriptions->remove($key);
-                return $this;
-            }
+   public function getSalt()
+   {
+      return '';
+   }
 
-        }
-        
-        return $this;
-    }
+   /**
+    * Set creationDate
+    *
+    * @param datetime $creationDate
+    */
+   protected function setCreationDate($creationDate)
+   {
+      $this->creationDate = $creationDate;
+   }
+
+   /**
+    * Get creationDate
+    *
+    * @return datetime 
+    */
+   public function getCreationDate()
+   {
+      return $this->creationDate;
+   }
+
+   /**
+    * Set confirmed
+    *
+    * @param boolean $confirmed
+    */
+   public function setConfirmed($confirmed)
+   {
+      $this->confirmed = $confirmed;
+   }
+
+   /**
+    * Get confirmed
+    *
+    * @return boolean 
+    */
+   public function getConfirmed()
+   {
+      return $this->confirmed;
+   }
+
+   /**
+    * 
+    * @param string $phonenumber
+    * @return \Progracqteur\WikipedaleBundle\Entity\Management\User
+    */
+   public function setPhonenumber($phonenumber)
+   {
+      $this->phonenumber = $phonenumber;
+      return $this;
+   }
+
+   /**
+    * 
+    * @return string
+    */
+   public function getPhonenumber()
+   {
+      return $this->phonenumber;
+   }    
+
+   /**
+    * Set infos
+    *
+    * @param Progracqteur\WikipedaleBundle\Resources\Container\Hash $infos
+    */
+   public function setInfos(Hash $infos)
+   {
+      $this->infos = $infos;
+   }
+
+   /**
+    * Get infos
+    *
+    * @return Progracqteur\WikipedaleBundle\Resources\Container\Hash 
+    */
+   public function getInfos()
+   {
+      return $this->infos;
+   }
+
+   /**
+    * Set nbComment
+    *
+    * 
+    */
+   public function increaseNbComment()
+   {
+      $this->nbComment++;
+   }
+
+   /**
+    * Get nbComment
+    *
+    * @return integer 
+    */
+   public function getNbComment()
+   {
+      return $this->nbComment;
+   }
+
+   /**
+    * Set nbVote
+    *
+    * 
+    */
+   public function increaseNbVote($nbVote)
+   {
+      $this->nbVote++;
+   }
+
+   /**
+    * Get nbVote
+    *
+    * @return integer 
+    */
+   public function getNbVote()
+   {
+      return $this->nbVote;
+   }
+
+   public function isRegistered()
+   {
+      return true;
+   }
+
+   public function setVirtual($virtual) {
+      $this->virtual = $virtual;
+      $this->setLocked($virtual);
+   }
+
+   public function isVirtual() {
+      return $this->virtual;
+   }
+
+   public function isVirtualConsistant(\Symfony\Component\Validator\ExecutionContextInterface $context) {
+      if ($this->isVirtual() === true ) {
+         if ($this->isLocked() === false ) {
+            $context->addViolationAt('locked', 
+               "admin.profile_user.inconsistent_virtual_lock", array(), null);
+         }
+      }
+   }
+
+   /**
+    * 
+    * @return Doctrine\Common\Collections\ArrayCollection
+    */
+   public function getNotificationSubscriptions()
+   {
+       return $this->notificationSubscriptions;
+   }
+
+   public function equals(UserInterface $user) {
+      if ($user instanceof UnregisteredUser)
+         return false;
+      else {
+         return $user->getId() === $this->getId();
+      }
+   }
+
+   /**
+    * 
+    * @param \Progracqteur\WikipedaleBundle\Entity\Management\NotificationSubscription $notification
+    * @return \Progracqteur\WikipedaleBundle\Entity\Management\User
+    */
+   public function addNotificationSubscription(NotificationSubscription $notification)
+   {
+      $this->notificationSubscriptions->add($notification);
+      return $this;
+   }
+
+   public function removeNotificationSubscription(NotificationSubscription $notification)
+   {
+      foreach($this->notificationSubscriptions as $key => $not) {
+         if ($not->getId() === $notification->getId()) {
+            $this->notificationSubscriptions->remove($key);
+            return $this;
+         }
+      }
+
+      return $this;
+   }
 }
