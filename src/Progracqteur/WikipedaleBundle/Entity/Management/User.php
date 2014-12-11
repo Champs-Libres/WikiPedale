a956fd755aa5414914ada5953b42da6ae10bd9be
Marc Ducobu <marc@champs-libres.coop>
2014-12-11 15:01:00 +0100

    Refacoring src/Progracqteur/WikipedaleBundle/Entity/Management/User.php : psr-i

--- src/Progracqteur/WikipedaleBundle/Entity/Management/User.php
+++ src/Progracqteur/WikipedaleBundle/Entity/Management/User.php
@@ -21,160 +21,158 @@ class User extends BaseUser
     protected $email = '';
 
     /**
-    * the way the user want to be publicly known
-    * 
-    * @var string $label
-    */
+     * the way the user want to be publicly known
+     * 
+     * @var string $label
+     */
     protected $label = '';
 
     /**
-    *
-    * @var string 
-    */
+     *
+     * @var string 
+     */
     protected $phonenumber = "";
 
     /**
-    * @var datetime $creationDate
-    */
+     * @var datetime $creationDate
+     */
     protected $creationDate;
 
     /**
-    * @var Progracqteur\WikipedaleBundle\Resources\Container\Hash $infos
-    */
+     * @var Progracqteur\WikipedaleBundle\Resources\Container\Hash $infos
+     */
     private $infos;
 
     /**
-    * @var integer $nbComment
-    */
+     * @var integer $nbComment
+     */
     private $nbComment = 0;
 
     /**
-    * @var integer $nbVote
-    */
+     * @var integer $nbVote
+     */
     private $nbVote = 0;
 
     /**
-    *
-    * @var boolean 
-    */
+     *
+     * @var boolean 
+     */
     private $virtual = false;
 
     /**
-    *
-    * @var Doctrine\Common\Collections\ArrayCollection
-    */
+     *
+     * @var Doctrine\Common\Collections\ArrayCollection
+     */
     private $notificationSubscriptions;
 
     const ROLE_ADMIN = 'ROLE_ADMIN';
 
     /**
-    * indicate if the user may create or alter categories on a report
-    * 
-    * @var string
-    */
-    const ROLE_CATEGORY = 'ROLE_CATEGORY'; 
+     * indicate if the user may create or alter categories on a report
+     * 
+     * @var string
+     */
+    const ROLE_CATEGORY = 'ROLE_CATEGORY';
 
     /**
-    * indicate if the user may create or alter notation. 
-    * The group which use it must be associated with a particular notation
-    * 
-    * @var string
-    */
+     * indicate if the user may create or alter notation. 
+     * The group which use it must be associated with a particular notation
+     * 
+     * @var string
+     */
     const ROLE_NOTATION = 'ROLE_NOTATION';
 
     /**
-    * @var string
-    */
+     * @var string
+     */
     const ROLE_MANAGER = 'ROLE_MANAGER';
 
     /**
-    * @var string
-    */
+     * @var string
+     */
     const ROLE_MODERATOR = 'ROLE_MODERATOR';
 
     /**
-    * indicate if the user may alter details of a little point
-    * 
-    * @var string
-    */
+     * indicate if the user may alter details of a little point
+     * 
+     * @var string
+     */
     const ROLE_DETAILS_LITTLE = 'ROLE_DETAILS_LITTLE';
 
     /**
-    * indicate if the user may alter details of a BIG point
-    * 
-    * @var string
-    */
+     * indicate if the user may alter details of a BIG point
+     * 
+     * @var string
+     */
     const ROLE_DETAILS_BIG = 'ROLE_DETAILS_BIG';
 
     /**
-    * 
-    * indicate if the user may publish/unpublish a report
-    * 
-    * @var string 
-    */
+     * indicate if the user may publish/unpublish a report
+     * 
+     * @var string 
+     */
     const ROLE_PUBLISHED = 'ROLE_PUBLISHED';
 
     /**
-    * indicate if the user may see email and personal
-    * details of other users
-    * 
-    * @var string
-    */
+     * indicate if the user may see email and personal
+     * details of other users
+     * 
+     * @var string
+     */
     const ROLE_SEE_USER_DETAILS = 'ROLE_SEE_USER_DETAILS';
 
     /**
-    * indicate if the user may alter or modify the manager
-    * of a report
-    * 
-    * @var string
-    */
+     * indicate if the user may alter or modify the manager
+     * of a report
+     * 
+     * @var string
+     */
     const ROLE_MANAGER_ALTER = 'ROLE_MANAGER_ALTER';
 
     /**
-    * indicate if the user may alter the report type
-    */
+     * indicate if the user may alter the report type
+     */
     const ROLE_REPORTTYPE_ALTER = 'ROLE_PLACETYPE_ALTER';
 
     /**
-    * indicate if the user may see unaccepted entities (like reports,
-    *  photos, comments, etc.)
-    */
+     * indicate if the user may see unaccepted entities (like reports,
+     *  photos, comments, etc.)
+     */
     const ROLE_SEE_UNACCEPTED = 'ROLE_SEE_UNACCEPTED';
 
     /**
-    * indicate if the user may alter the moderator's comment
-    * the moderator's comment is inside a report: this is not a comment entity
-    */
+     * indicate if the user may alter the moderator's comment
+     * the moderator's comment is inside a report: this is not a comment entity
+     */
     const ROLE_MODERATOR_COMMENT_ALTER = 'ROLE_MODERATOR_COMMENT_ALTER';
 
     /**
-    * indicate if the user may add and see private comments between
-    * moderators and manager. This concern comment entity and not 
-    * report's moderator comment
-    */
+     * indicate if the user may add and see private comments between
+     * moderators and manager. This concern comment entity and not 
+     * report's moderator comment
+     */
     const ROLE_COMMENT_MODERATOR_MANAGER = 'ROLE_COMMENT_MODERATOR_MANAGER';
 
     /**
-    * indicate if the user may alter the term.
-    */
-
+     * indicate if the user may alter the term.
+     */
     const ROLE_REPORT_TERM = 'ROLE_PLACE_TERM';
-   
-   /**
-    * indicate if the user/group may change the moderator
-    */
-   const ROLE_MODERATOR_ALTER = 'ROLE_MODERATOR_ALTER';
-   
+
     /**
-    * Creates a new user
-    * @param array of parameters :
-    * - 'label' => the label of the user (if equals to 'RANDOM' : generation of a random label)
-    * - 'username' => the username of the user (if equals to 'RANDOM' : generation of a random label)
-    * - 'password' => the  password of the user.
-    * - 'email' => the email of the user (if equals to 'RANDOM' : genration of a random email)
-    * - 'phonenumber' => the phonenumber of the user (if equals to 'RANDOM' : generation of a random phonumber)
-    * @return  Progracqteur\WikipedaleBundle\Entity\Model\User
-    */
+     * indicate if the user/group may change the moderator
+     */
+    const ROLE_MODERATOR_ALTER = 'ROLE_MODERATOR_ALTER';
+
+    /**
+     * Creates a new user
+     * @param array of parameters :
+     * - 'label' => the label of the user (if equals to 'RANDOM' : generation of a random label)
+     * - 'username' => the username of the user (if equals to 'RANDOM' : generation of a random label)
+     * - 'password' => the  password of the user.
+     * - 'email' => the email of the user (if equals to 'RANDOM' : genration of a random email)
+     * - 'phonenumber' => the phonenumber of the user (if equals to 'RANDOM' : generation of a random phonumber)
+     * @return  Progracqteur\WikipedaleBundle\Entity\Model\User
+     */
     public function __construct($params = array())
     {
         parent::__construct();
@@ -183,7 +181,7 @@ class User extends BaseUser
         $this->notificationSubscriptions = new ArrayCollection();
 
         if (array_key_exists('label', $params)) {
-            if($params['label'] === 'RANDOM') {
+            if ($params['label'] === 'RANDOM') {
                 $this->setLabel(StringGenerator::randomGenerate(10));
             } else {
                 $this->setLabel($params['label']);
@@ -191,7 +189,7 @@ class User extends BaseUser
         }
 
         if (array_key_exists('username', $params)) {
-            if($params['username'] === 'RANDOM') {
+            if ($params['username'] === 'RANDOM') {
                 $this->setUsername(StringGenerator::randomGenerate(10));
             } else {
                 $this->setUsername($params['username']);
@@ -203,7 +201,7 @@ class User extends BaseUser
         }
 
         if (array_key_exists('email', $params)) {
-            if($params['email'] === 'RANDOM') {
+            if ($params['email'] === 'RANDOM') {
                 $this->setEmail((StringGenerator::randomGenerate(6) . '@blopblop.be'));
             } else {
                 $this->setEmail($params['email']);
@@ -211,32 +209,32 @@ class User extends BaseUser
         }
 
         if (array_key_exists('phonenumber', $params)) {
-            if($params['phonenumber'] === 'RANDOM') {
+            if ($params['phonenumber'] === 'RANDOM') {
                 $this->setPhonenumber(StringGenerator::randomGenerate(10));
             } else {
                 $this->setPhonenumber($params['phonenumber']);
             }
         }
 
-        if(array_key_exists('enable', $params)) {
+        if (array_key_exists('enable', $params)) {
             $this->setEnabled($params['enable']);
         }
-   }
+    }
 
     /**
-    * Get id
-    *
-    * @return integer 
-    */
+     * Get id
+     *
+     * @return integer 
+     */
     public function getId()
     {
         return $this->id;
     }
 
     /**
-    * Set label
-    * @param string $label
-    */
+     * Set label
+     * @param string $label
+     */
     public function setLabel($label)
     {
         $this->label = $label;
@@ -244,20 +242,20 @@ class User extends BaseUser
     }
 
     /**
-    * Get label
-    *
-    * @return string 
-    */
+     * Get label
+     *
+     * @return string 
+     */
     public function getLabel()
     {
         return $this->label;
     }
 
     /**
-    * Set creationDate
-    *
-    * @param datetime $creationDate
-    */
+     * Set creationDate
+     *
+     * @param datetime $creationDate
+     */
     protected function setCreationDate($creationDate)
     {
         $this->creationDate = $creationDate;
@@ -265,20 +263,20 @@ class User extends BaseUser
     }
 
     /**
-    * Get creationDate
-    *
-    * @return datetime 
-    */
+     * Get creationDate
+     *
+     * @return datetime 
+     */
     public function getCreationDate()
     {
         return $this->creationDate;
     }
 
     /**
-    * Set confirmed
-    *
-    * @param boolean $confirmed
-    */
+     * Set confirmed
+     *
+     * @param boolean $confirmed
+     */
     public function setConfirmed($confirmed)
     {
         $this->confirmed = $confirmed;
@@ -286,20 +284,20 @@ class User extends BaseUser
     }
 
     /**
-    * Get confirmed
-    *
-    * @return boolean 
-    */
+     * Get confirmed
+     *
+     * @return boolean 
+     */
     public function getConfirmed()
     {
         return $this->confirmed;
     }
 
     /**
-    * 
-    * @param string $phonenumber
-    * @return \Progracqteur\WikipedaleBundle\Entity\Management\User
-    */
+     * 
+     * @param string $phonenumber
+     * @return \Progracqteur\WikipedaleBundle\Entity\Management\User
+     */
     public function setPhonenumber($phonenumber)
     {
         $this->phonenumber = $phonenumber;
@@ -307,19 +305,19 @@ class User extends BaseUser
     }
 
     /**
-    * 
-    * @return string
-    */
+     * 
+     * @return string
+     */
     public function getPhonenumber()
     {
         return $this->phonenumber;
-    }    
+    }
 
     /**
-    * Set infos
-    *
-    * @param Progracqteur\WikipedaleBundle\Resources\Container\Hash $infos
-    */
+     * Set infos
+     *
+     * @param Progracqteur\WikipedaleBundle\Resources\Container\Hash $infos
+     */
     public function setInfos(Hash $infos)
     {
         $this->infos = $infos;
@@ -327,20 +325,20 @@ class User extends BaseUser
     }
 
     /**
-    * Get infos
-    *
-    * @return Progracqteur\WikipedaleBundle\Resources\Container\Hash 
-    */
+     * Get infos
+     *
+     * @return Progracqteur\WikipedaleBundle\Resources\Container\Hash 
+     */
     public function getInfos()
     {
         return $this->infos;
     }
 
     /**
-    * Set nbComment
-    *
-    * 
-    */
+     * Set nbComment
+     *
+     * 
+     */
     public function increaseNbComment()
     {
         $this->nbComment++;
@@ -348,20 +346,20 @@ class User extends BaseUser
     }
 
     /**
-    * Get nbComment
-    *
-    * @return integer 
-    */
+     * Get nbComment
+     *
+     * @return integer 
+     */
     public function getNbComment()
     {
         return $this->nbComment;
     }
 
     /**
-    * Set nbVote
-    *
-    * 
-    */
+     * Set nbVote
+     *
+     * 
+     */
     public function increaseNbVote($nbVote)
     {
         $this->nbVote++;
@@ -369,10 +367,10 @@ class User extends BaseUser
     }
 
     /**
-    * Get nbVote
-    *
-    * @return integer 
-    */
+     * Get nbVote
+     *
+     * @return integer 
+     */
     public function getNbVote()
     {
         return $this->nbVote;
@@ -383,47 +381,50 @@ class User extends BaseUser
         return true;
     }
 
-    public function setVirtual($virtual) {
+    public function setVirtual($virtual)
+    {
         $this->virtual = $virtual;
         $this->setLocked($virtual);
         return $this;
     }
 
-    public function isVirtual() {
+    public function isVirtual()
+    {
         return $this->virtual;
     }
 
-    public function isVirtualConsistant(\Symfony\Component\Validator\ExecutionContextInterface $context) {
-        if ($this->isVirtual() === true ) {
-            if ($this->isLocked() === false ) {
-                $context->addViolationAt('locked', 
-                    "admin.profile_user.inconsistent_virtual_lock", array(), null);
+    public function isVirtualConsistant(\Symfony\Component\Validator\ExecutionContextInterface $context)
+    {
+        if ($this->isVirtual() === true) {
+            if ($this->isLocked() === false) {
+                $context->addViolationAt('locked', "admin.profile_user.inconsistent_virtual_lock", array(), null);
             }
         }
     }
 
     /**
-    * 
-    * @return Doctrine\Common\Collections\ArrayCollection
-    */
+     * 
+     * @return Doctrine\Common\Collections\ArrayCollection
+     */
     public function getNotificationSubscriptions()
     {
         return $this->notificationSubscriptions;
     }
 
-    public function equals(UserInterface $user) {
-        if ($user instanceof UnregisteredUser)
+    public function equals(UserInterface $user)
+    {
+        if ($user instanceof UnregisteredUser) {
             return false;
-        else {
+        } else {
             return $user->getId() === $this->getId();
         }
     }
 
     /**
-    * 
-    * @param \Progracqteur\WikipedaleBundle\Entity\Management\NotificationSubscription $notification
-    * @return \Progracqteur\WikipedaleBundle\Entity\Management\User
-    */
+     * 
+     * @param \Progracqteur\WikipedaleBundle\Entity\Management\NotificationSubscription $notification
+     * @return \Progracqteur\WikipedaleBundle\Entity\Management\User
+     */
     public function addNotificationSubscription(NotificationSubscription $notification)
     {
         $this->notificationSubscriptions->add($notification);
@@ -432,7 +433,7 @@ class User extends BaseUser
 
     public function removeNotificationSubscription(NotificationSubscription $notification)
     {
-        foreach($this->notificationSubscriptions as $key => $not) {
+        foreach ($this->notificationSubscriptions as $key => $not) {
             if ($not->getId() === $notification->getId()) {
                 $this->notificationSubscriptions->remove($key);
                 return $this;
@@ -443,8 +444,8 @@ class User extends BaseUser
     }
 
     public function addGroup(\FOS\UserBundle\Model\GroupInterface $group)
-    { 
-      if ( ! $this->getGroups()->contains($group)) {
+    {
+        if (!$this->getGroups()->contains($group)) {
             parent::addGroup($group);
 
             //add a notification subscription if group of type moderator or manager
@@ -459,7 +460,7 @@ class User extends BaseUser
                     break;
                 default:
                     $notification = null;
-                break;
+                    break;
             }
 
             if ($notification !== null) {
@@ -471,7 +472,7 @@ class User extends BaseUser
                 $this->addNotificationSubscription($notification);
             }
         }
-      
+
         return $this;
     }
 
@@ -479,19 +480,17 @@ class User extends BaseUser
     {
         if ($this->getGroups()->contains($group)) {
             parent::removeGroup($group);
-         
+
             //remove the notification if group of type moderator or manager
-            if (in_array($group->getType(), 
-                array( Group::TYPE_MANAGER or Group::TYPE_MODERATOR))) {
+            if (in_array($group->getType(), array(Group::TYPE_MANAGER or Group::TYPE_MODERATOR))) {
                 foreach ($this->getNotificationSubscriptions() as $notification) {
-                    if ($notification->hasGroup() && 
-                       $notification->getGroup()->getId() === $group->getId()) {
+                    if ($notification->hasGroup() &&
+                        $notification->getGroup()->getId() === $group->getId()) {
                         $this->removeNotificationSubscription($notification);
                     }
                 }
             }
         }
-    }  
+    }
 
 }
-
