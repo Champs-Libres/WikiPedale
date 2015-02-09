cf5f4d909284bc8774f8a05a9aef01de86b4365b
Marc Ducobu <marc@champs-libres.coop>
2015-02-09 22:42:36 +0100

    Plain password -> sha1 - close #366

---
+++ src/Progracqteur/WikipedaleBundle/Command/FixPlainPasswordToHashCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Progracqteur\WikipedaleBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Progracqteur\WikipedaleBundle\Entity\Management\User;
+
+/**
+ * Set salt into password
+ */
+class FixPlainPasswordToHashCommand extends ContainerAwareCommand {
+    protected function configure()
+    {
+        $this->setName('uello:fixPlainPasswordToHashCommand')
+            ->setDescription("Password : plaintext -> hash (as in security.yml)");
+    }
+    
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $manager = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $users = $manager->getRepository('ProgracqteurWikipedaleBundle:Management\User')
+            ->findAll();
+        
+        foreach ($users as $user) {
+            $userPassword = $user->getPassword();
+            $saltSize = strlen($user->getSalt());
+            $plainPassword = substr($userPassword, 0, strlen($userPassword) - $saltSize -2);
+
+            $encoder = $this->getContainer()->get('security.encoder_factory')->getEncoder($user);
+            $encodedPass = $encoder->encodePassword($plainPassword, $user->getSalt());
+            $user->setPassword($encodedPass);
+        }
+        $manager->flush();   
+    }
+}
