f9c405a195e521019d6a4ea947f2d992e81b476f
Marc Ducobu <marc@champs-libres.coop>
2015-04-20 22:03:30 +0200

    jslint correction

--- web/js/report_printable.js
+++ web/js/report_printable.js
@@ -1,56 +1,60 @@
+/* jslint vars: true */
+/*jslint indent: 4 */ // passer à 3?
+/* global ol, web_dir */
+'use strict';
+
 var reportPrintable = {};
 
 reportPrintable.getColor = function(statusInt) {
     switch (statusInt) {
-        case 0: return 'w';
-        case -1: return 'd';
-        case 1: return 'r';
-        case 2: return 'o';
-        case 3: return 'g';
+        case 0:
+            return 'w';
+        case -1:
+            return 'd';
+        case 1:
+            return 'r';
+        case 2:
+            return 'o';
+        case 3:
+            return 'g';
     }
-}
-
-
+};
 
 reportPrintable.launchMap = function(div, lon, lat, status) {
 
     var iconFeature = new ol.Feature({
-      geometry: new ol.geom.Point(ol.proj.transform([lon, lat], 'EPSG:4326', 'EPSG:3857'))
+        geometry: new ol.geom.Point(ol.proj.transform([lon, lat], 'EPSG:4326', 'EPSG:3857'))
     });
 
-    console.log( web_dir + 'img/OpenLayers/m_' + this.getColor(status) + '.png');
     var iconStyle = new ol.style.Style({
-         image: new ol.style.Icon(({
+        image: new ol.style.Icon(({
             src:web_dir + 'img/OpenLayers/m_' + this.getColor(status) + '.png',
             scale: 0.75,
             anchor: [0.5,1]
-         }))
+        }))
     });
 
     iconFeature.setStyle(iconStyle);
 
     var vectorSource = new ol.source.Vector({
-      features: [iconFeature]
+        features: [iconFeature]
     });
 
     var vectorLayer = new ol.layer.Vector({
-      source: vectorSource
+        source: vectorSource
     });
 
-    map = new ol.Map({
+    new ol.Map({
         target: div,
         layers: [
-          new ol.layer.Tile({
-            source: new ol.source.OSM()
-          }),
-          vectorLayer
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            }),
+            vectorLayer
         ],
         view: new ol.View({
-          center: ol.proj.transform([lon, lat], 'EPSG:4326', 'EPSG:3857'),
-          zoom: 18
+            center: ol.proj.transform([lon, lat], 'EPSG:4326', 'EPSG:3857'),
+            zoom: 18
         })
-      });
-      
-}
-
-
+    });
+};
