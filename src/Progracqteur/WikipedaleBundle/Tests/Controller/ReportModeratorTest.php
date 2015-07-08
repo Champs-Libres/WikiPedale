9f1b26e06728d67fe1ca758a9e1091e0b8b0e481
Marc Ducobu <marc@champs-libres.coop>
2015-07-08 11:25:56 +0200

    Adding license comment

--- src/Progracqteur/WikipedaleBundle/Tests/Controller/ReportModeratorTest.php
+++ src/Progracqteur/WikipedaleBundle/Tests/Controller/ReportModeratorTest.php
@@ -1,5 +1,25 @@
 <?php
 
+/*
+ *  Uello is a reporting tool. This file is part of Uello.
+ * 
+ *  Copyright (C) 2015, Champs-Libres Cooperative SCRLFS,
+ *  <http://www.champs-libres.coop>, <info@champs-libres.coop>
+ * 
+ *  Uello is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Uello is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Uello.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 namespace Progracqteur\WikipedaleBundle\Tests\Controller;
 
 ini_set('memory_limit', '256M');
